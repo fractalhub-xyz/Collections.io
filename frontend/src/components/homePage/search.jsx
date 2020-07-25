401749a3d7c1174230c9339e207e81e432808db8
Ajay051198 <ajay.uom@gmail.com>
2020-07-25 13:50:28 +0530

    cleaning up code

--- frontend/src/components/homePage/search.jsx
+++ frontend/src/components/homePage/search.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { getSearchResults } from "../../helpers/api";
-import { useHistory } from "react-router-dom";
 import { randomColor } from "randomcolor";
 import SnippetSearch from "./snippetSearch";
 import CollectionSearch from "./searchCollection";
@@ -11,12 +10,8 @@ function Search({ searchText }) {
   const [snippetMatches, setSnippetMatches] = useState([]);
   const [tagMatches, setTagMatches] = useState([]);
   const [numResults, setNumResults] = useState(0);
+  const [success, setSuccess] = useState(false)
   //utility fucntions
-  let history = useHistory();
-  const openCollection = (id) => {
-    history.push("/detail/" + id);
-  };
-
   const color = randomColor({
     luminosity: "light",
     hue: "rgba",
@@ -38,6 +33,7 @@ function Search({ searchText }) {
         response.data.result.collections.length +
           response.data.result.snippets.length
       );
+      setSuccess(true);
     } catch (error) {
       console.log(error);
     }
@@ -45,7 +41,11 @@ function Search({ searchText }) {
   return (
     <div className={!!searchText.length ? "container" : "container dispnone"}>
       <div onClick={Search}>Search</div>
-      <h1>{!!collectionMatches.length && <span>{numResults} Results</span>}</h1>
+      <h1>
+        {success && (
+          <span>{numResults} Results</span>
+        )}
+      </h1>
 
       {!!collectionMatches.length && (
         <div className="sectionLG">
