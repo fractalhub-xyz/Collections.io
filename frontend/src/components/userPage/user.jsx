345ea3be173aecd457a5cbe555635afbd22749d2
Ajay051198 <ajay.uom@gmail.com>
2020-08-01 10:54:56 +0530

    added elements to user view

--- frontend/src/components/userPage/user.jsx
+++ frontend/src/components/userPage/user.jsx
@@ -4,9 +4,13 @@ import { useParams } from "react-router-dom";
 //components
 import SideNav from "../homePage/sidenav";
 import Navbar from "../detailPage/navbar";
-
+import Carousel from "../homePage/carousel";
+import Collections from "../homePage/collections";
 //API
 import { getUserFromID } from "../../helpers/api";
+//CSS
+import "./user.css";
+
 function User() {
   // params
   const params = useParams();
@@ -40,24 +44,57 @@ function User() {
     fetchUserInfo();
     setRefresh(false);
   }, [refresh]);
+
+  const refreshCount = () => {
+    var colLen = userCollectinos.length;
+    var likes = 0;
+    for (var i = 0; i < colLen; i++) {
+      var snipLen = userCollectinos[i].snippets.length;
+      console.log(userCollectinos[i].name);
+      if (snipLen !== 0) {
+        for (var j = 0; j < snipLen; j++) {
+          likes = likes + userCollectinos[i].snippets[j].hearts.length;
+        }
+        console.log(likes);
+        likes = 0;
+      }
+    }
+  };
+
+  useEffect(() => {
+    refreshCount();
+  }, [userCollectinos]);
+
   return (
     <div className="root">
       <SideNav setRefresh={setRefresh} />
       <div className="main">
         <Navbar />
-        {error ? (
-          <div className="loading-error">{error}</div>
-        ) : (
-          <div className="container">
-            <div>{user.username}</div>
-            <div>{user.email}</div>
-            <ul>
-              {userCollectinos.map((collection) => (
-                <li>{collection.name} -- Number of snippets {collection.snippets.length}</li>
-              ))}
-            </ul>
+
+        <div className="container">
+          <h1 className="username">{user.username}</h1>
+          <div className="stats-container">
+            <div className="stat">
+              <h1>-</h1>
+              <span>Followers</span>
+            </div>
+            <div className="stat">
+              <h1>{userCollectinos.length}</h1>
+              <span>Collections</span>
+            </div>
+            <div className="stat">
+              <h1>32</h1>
+              <span>Snippets</span>
+            </div>
           </div>
-        )}
+          {error && <h4>{error}</h4>}
+          {isLoading && <h4>Loading..</h4>}
+          <Carousel title="Your Collections">
+            {userCollectinos.map((collection) => (
+              <Collections key={collection.id} collection={collection} />
+            ))}
+          </Carousel>
+        </div>
       </div>
     </div>
   );
