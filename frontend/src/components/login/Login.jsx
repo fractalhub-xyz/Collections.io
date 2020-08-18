8c228021abf0129905a2de62b68796688d64d8a6
Ajay051198 <ajay.uom@gmail.com>
2020-08-18 23:00:35 +0530

    added global state management boilerplate

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -2,15 +2,32 @@ import React from "react";
 import "./login.sass";
 //components
 import LoginForm from "./loginForm";
-
+import { useStateValue } from "../../helpers/stateProvider";
 // import cx from "../../helpers/cx";
 
 function Login() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <main className="login">
       <header>This should be aqua yellow</header>
       <section>
         <LoginForm />
+        <p>{user}</p>
+        <button
+          onClick={() => {
+            dispatch({ type: "SET_USER", user: "admin" });
+          }}
+        >
+          set
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "SET_USER", user: "not admin" });
+          }}
+        >
+          reset
+        </button>
       </section>
     </main>
   );
