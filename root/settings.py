e4076797417d1c4d5ff7147c6b23e8fa5a9dc3c6
digi0ps <sriru1998@gmail.com>
2020-08-22 22:55:33 +0530

    Use database_url.

--- root/settings.py
+++ root/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,6 +94,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
