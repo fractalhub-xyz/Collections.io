3b6945076c18837a7bf240d53265ad0a0e25ec41
Ajay051198 <ajay.uom@gmail.com>
2020-09-01 12:28:12 +0530

    fixed login form

--- frontend/src/components/login/loginForm.jsx
+++ frontend/src/components/login/loginForm.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+//api
+import { postLogin } from "../../helpers/api";
 
 function LoginForm() {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  // };
+
+  const loginReq = async (data) => {
+    const payload = {
+      username: data.username,
+      password: data.password,
+    };
+    try {
+      const response = await postLogin(payload);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", data.username);
+    } catch {
+      alert("failed // temp");
+    }
   };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(loginReq)}>
       <h1>Login</h1>
       <label>Username</label>
       <input name="username" ref={register({ required: true })} />
@@ -22,7 +40,7 @@ function LoginForm() {
             message: "This is a error message (required field)",
             value: true,
           },
-          maxLength: { value: 2, message: "isa too big" },
+          maxLength: { value: 200, message: "isa too big" },
         })}
       />
       <p>{errors?.password?.message}</p>
